3971148bc424bef16805645cb28a9eba6b9d79ae
Dahyeon Baek <101542378+Bdhyeon@users.noreply.github.com>
2022-07-26 22:57:28 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 840 ms, Memory: 61364 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -2,33 +2,30 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static Comparator<Integer[]> comp = new Comparator<Integer[]>() {
-		public int compare(Integer[] o1, Integer[] o2) {
-			if(o1[0].equals(o2[0])) {
-				return o1[1].compareTo(o2[1]);
-			}else {
-				return o1[0].compareTo(o2[0]);
-			}
-		};
-	};
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int n = Integer.parseInt(br.readLine());
+		int[][] spot = new int[n][2];
 		
-		ArrayList<Integer[]> spot = new ArrayList<Integer[]>();
 		for(int i=0; i<n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			spot.add(new Integer[]{x,y});
+			spot[i][0] = Integer.parseInt(st.nextToken());
+			spot[i][1] = Integer.parseInt(st.nextToken());
 		}
 		
-		spot.sort(comp);
-		for(int i=0; i<spot.size(); i++) {
-			bw.write(String.valueOf(spot.get(i)[0] + " " + spot.get(i)[1]) + "\n");
+		Arrays.sort(spot, (s1, s2) ->{
+			if(s1[0] == s2[0]) {
+				return s1[1]-s2[1];
+			}else {
+				return s1[0]-s2[0];
+			}
+		});
+		
+		for(int[] data : spot) {
+			bw.write(String.valueOf(data[0])+" "+String.valueOf(data[1]) + "\n");
 		}
 		
 		bw.flush();
