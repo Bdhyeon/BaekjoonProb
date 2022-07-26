7c8137566e111e4407d28669ca5c06ab9a8b4a0b
Dahyeon Baek <101542378+Bdhyeon@users.noreply.github.com>
2022-07-26 23:02:55 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 812 ms, Memory: 61876 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int n = Integer.parseInt(br.readLine());
+		int[][] spot = new int[n][2];
+		
+		for(int i=0; i<n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			spot[i][0] = Integer.parseInt(st.nextToken());
+			spot[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(spot, (s1, s2) ->{
+			if(s1[1] == s2[1]) {
+				return s1[0] - s2[0];
+			}else {
+				return s1[1] - s2[1];
+			}
+		});
+		
+		for(int[] data : spot) {
+			bw.write(String.valueOf(data[0])+" "+String.valueOf(data[1]) + "\n");
+		}
+		
+		bw.flush();
+		br.close();
+		bw.close();
+	}// end main()
+}
