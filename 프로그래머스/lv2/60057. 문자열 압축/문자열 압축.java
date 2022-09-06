68178f0cdc5d7f0e57f96198c92c1888b38bb237
Dahyeon Baek <101542378+Bdhyeon@users.noreply.github.com>
2022-09-06 23:33:33 +0900

    [level 2] Title: 문자열 압축, Time: 0.08 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.java
@@ -5,30 +5,31 @@ class Solution {
         
         for(int i=1; i<=length/2; i++){
             String copyS = s;
-            String resultS = "";
+            StringBuilder resultS = new StringBuilder();
             int cnt = 1; //반복되는 횟수 카운트
             
             while(copyS.length()>i){
                 String subsS = copyS.substring(0, i);
                 copyS = copyS.substring(i);
-                //System.out.println("i=" + i + " s=" + subsS + " c=" + copyS);
                 
                 //압축되는 문자열이 있을 경우 반복횟수+1, 없을경우 횟수 초기화
                 if(copyS.startsWith(subsS)){
                     cnt++;
                     if(copyS.equals(subsS)){
-                        resultS += cnt;
-                        resultS += copyS;
+                        resultS.append(cnt).append(copyS);
                     }
                 }else{
                     if(cnt>1)
-                        resultS += cnt;
-                    resultS = copyS.length()>i ? resultS + subsS : resultS + subsS + copyS;
+                        resultS.append(cnt);
+                    
+                    resultS.append(subsS);
+                    
+                    if(copyS.length()<=i)
+                        resultS.append(copyS);
                     cnt = 1;
                 }
             }
             answer = Math.min(answer, resultS.length());
-            //System.out.println("i=" + i + " test=" + resultS + " 길이=" + resultS.length());
         }
         
         return answer;
