b69f3f07fbb65f001ea113494f5c28b5f25ae040
Dahyeon Baek <101542378+Bdhyeon@users.noreply.github.com>
2022-09-13 18:47:10 +0900

    [level 2] Title: 숫자의 표현, Time: 0.08 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -2,17 +2,10 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        for(int i=1; i<=n; i++){
-            if(n%2 != 0){
-                if(n%i == 0){
-                    answer++;
-                }
-            }else{
-                if(i%2 != 0 && n%i == 0){
-                    answer++;
-                }
-            }
-            
+        for(int i=1; i<=n; i+=2){
+             if(n%i == 0){
+                answer++;
+             }
         }
         
         return answer;
